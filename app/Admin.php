8acee9f44fc32ac036abb4111eb32e14e13bb1d3
Hana7 <h.zewdie@kpnmail.nl>
2017-12-06 11:41:53 +0100

    Admin model

---
+++ app/Admin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'job_title'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    public function sendPasswordResetNotification($token)
+            {
+        $this->notify(new UserResetPasswordNotification($token));
+            }
+
+public function profiles()
+            {
+    return $this->belongsTo(Profile::class);
+            }
+
+    
+}
